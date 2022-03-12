8c6f39c993fdf970a01c01ec35332858578ae1e1
decabitrunner <76393568+decabitrunner@users.noreply.github.com>
2022-03-12 20:53:11 +0100

    the code

--- main.py
+++ main.py
@@ -10,17 +10,17 @@ def click(x,y):
     win32api.SetCursorPos((x, y))
     win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN, 0, 0)
     time.sleep(0.0001)
-y = 500
+y = 700
 while keyboard.is_pressed('q') == False:
 
-    if ag.pixel(745, 700)[0]  == 0:
-        click(745, 700)
+    if ag.pixel(745, y)[0]  == 0:
+        click(745, y)
 
-    if ag.pixel(876, 700)[0] == 0:
-        click(876, 700)
+    if ag.pixel(876, y)[0] == 0:
+        click(876, y)
 
-    if ag.pixel(1046, 700)[0] == 0:
-        click(1046, 700)
+    if ag.pixel(1046, y)[0] == 0:
+        click(1046, y)
 
-    if ag.pixel(1212, 700)[0] == 0:
-        click(1212, 700)
+    if ag.pixel(1212, y)[0] == 0:
+        click(1212, y)
