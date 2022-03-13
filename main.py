515759c265efacf1665952258f3ff976198cd7bd
decabitrunner <76393568+decabitrunner@users.noreply.github.com>
2022-03-13 08:21:52 +0100

    the code

--- main.py
+++ main.py
@@ -4,12 +4,14 @@ import time
 import keyboard
 import win32api, win32con
 
-time.sleep(3)
+
 
 def click(x,y):
     win32api.SetCursorPos((x, y))
     win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN, 0, 0)
     time.sleep(0.0001)
+
+time.sleep(3)
 y = 700
 while keyboard.is_pressed('q') == False:
 
